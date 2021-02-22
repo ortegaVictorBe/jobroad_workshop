43cfb9e6079f83a73530b05b7b0065312c016c33
Ortega <ortega.victor@gmail.com>
2021-02-23 00:25:37 +0100

    fix click repeat over pics

--- script/whodidit.js
+++ script/whodidit.js
@@ -15,7 +15,7 @@ const btnMessage = document.getElementById("btnMessage");
 const indxTool01 = document.getElementById("tool01");
 const indxTool02 = document.getElementById("tool02");
 
-//Quesiotn object
+//Question object
 const questions = [
     "Quien es Fumador?",
     "Quien es Bohemio?",
@@ -27,16 +27,22 @@ const questions = [
 
 var numberQuestion = 0;
 
-drawQuestion = () => {
-    if (numberQuestion <= 5) {
-        if (numberQuestion == 5) {
-            //Show button message
-            document.getElementById("questionText").innerHTML = "";
-            btnMessage.style.display = "inline";
+//Variables to control theat the images has to be clicked once
+var clicks = [0, 0, 0, 0, 0, 0];
 
-        } else {
-            document.getElementById("questionText").innerHTML = questions[numberQuestion];
-            numberQuestion = numberQuestion + 1;
+//Function to show the question
+drawQuestion = (imageclicked) => {
+    if (imageclicked == 0) {
+        if (numberQuestion <= 5) {
+            if (numberQuestion == 5) {
+                //Show button message
+                document.getElementById("questionText").innerHTML = "";
+                btnMessage.style.display = "inline";
+
+            } else {
+                document.getElementById("questionText").innerHTML = questions[numberQuestion];
+                numberQuestion = numberQuestion + 1;
+            }
         }
     }
 }
@@ -44,28 +50,35 @@ drawQuestion = () => {
 
 people01.onclick = () => {
     people01.src = "../img/hideImg01.jpg";
-    drawQuestion(numberQuestion);
+    drawQuestion(clicks[0]);
+    clicks[0] = 1;
 }
 people02.onclick = () => {
     people02.src = "../img/hideImg01.jpg";
-    drawQuestion(numberQuestion);
+    drawQuestion(clicks[1]);
+    clicks[1] = 1;
 }
 people03.onclick = () => {
     people03.src = "../img/hideImg01.jpg";
-    drawQuestion(numberQuestion);
+    drawQuestion(clicks[2]);
+    clicks[2] = 1;
+
 }
 
 people04.onclick = () => {
     people04.src = "../img/hideImg02.jpg";
-    drawQuestion(numberQuestion);
+    drawQuestion(clicks[3]);
+    clicks[3] = 1;
 }
 people05.onclick = () => {
     people05.src = "../img/hideImg02.jpg";
-    drawQuestion(numberQuestion);
+    drawQuestion(clicks[4]);
+    clicks[4] = 1;
 }
 people06.onclick = () => {
     people06.src = "../img/hideImg02.jpg";
-    drawQuestion(numberQuestion);
+    drawQuestion(clicks[5]);
+    clicks[5] = 1;
 }
 
 //reloading the pictures
@@ -80,6 +93,9 @@ btnReload.onclick = () => {
     //Reset Questions
     numberQuestion = 0;
     drawQuestion(numberQuestion);
+    btnMessage.style.display = "none";
+    clicks = [0, 0, 0, 0, 0, 0];
+
 }
 
 btnMessage.onclick = () => {
