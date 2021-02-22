7ed91f981f138392225af6498267684b079a01e6
Ortega <ortega.victor@gmail.com>
2021-02-23 00:04:55 +0100

    update background tile and overlay

--- script/whodidit_message.js
+++ script/whodidit_message.js
@@ -4,11 +4,6 @@
 const btnReload = document.getElementById("reload");
 const btnStart = document.getElementById("start");
 
-
-//Get indexelements
-const indxTool01 = document.getElementById("tool01");
-const indxTool02 = document.getElementById("tool02");
-
 //Sending to homePage
 btnStart.onclick = () => {
     window.open("index.html", "_self");
